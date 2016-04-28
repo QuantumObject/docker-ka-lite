bc4a577ddf5c57c687f07182bed78e067abe8181
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-04-28 02:59:51 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,8 +5,9 @@ set -e
 
 if [ -f /etc/configured ]; then
         echo 'already configured'
+        service ka-lite start
 else
-      
+        service ka-lite start 
         update-locale
         date > /etc/configured
 fi
