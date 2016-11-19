3cfefafa5f72202b89d0efe7f843367ebbb6f4c7
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-11-19 12:55:25 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,10 +6,10 @@ set -e
 if [ -f /etc/configured ]; then
         echo 'already configured'
         #service ka-lite start
-        chpst -u ka-lite /usr/bin/kalite start --port=8008
+        #chpst -u ka-lite /usr/bin/kalite start --port=8008
 else
         chown -R ka-lite:nogroup /var/ka-lite/.kalite
-        chpst -u ka-lite /usr/bin/kalite start --port=8008
+        #chpst -u ka-lite /usr/bin/kalite start --port=8008
         update-locale
         date > /etc/configured
 fi
